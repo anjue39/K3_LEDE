e69ea90a872f7ceb0258d52e8fd819fcb7c80e24
anjue39 <98279987+anjue39@users.noreply.github.com>
2022-09-16 06:14:38 +0800

    Create diy-part-22.sh

---
+++ diy-part-22.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Description: 大部分是一开始不在默认底包，feeds update和自定义添加完后才有的 后设置
+
+
+echo '修改主机名'
+sed -i "s/hostname='OpenWrt'/hostname='PHICOMM'/g" package/base-files/files/bin/config_generate
+cat package/base-files/files/bin/config_generate |grep hostname=
+echo '=========Alert hostname OK!========='
+
+echo '修改路由器默认IP'
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+echo '=========Alert default IP OK!========='
+
+# 修改插件名字
+# sed -i 's/"aMule设置"/"电驴下载"/g' `grep "aMule设置" -rl ./`
+# sed -i 's/"网络存储"/"NAS"/g' `grep "网络存储" -rl ./`
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `grep "Turbo ACC 网络加速" -rl ./`
+# sed -i 's/"实时流量监测"/"流量"/g' `grep "实时流量监测" -rl ./`
+# sed -i 's/"KMS 服务器"/"KMS激活"/g' `grep "KMS 服务器" -rl ./`
+# sed -i 's/"TTYD 终端"/"命令窗"/g' `grep "TTYD 终端" -rl ./`
+sed -i 's/"USB 打印服务器"/"打印服务"/g' `grep "USB 打印服务器" -rl ./`
+# sed -i 's/"Web 管理"/"Web"/g' `grep "Web 管理" -rl ./`
+# sed -i 's/"管理权"/"改密码"/g' `grep "管理权" -rl ./`
+# sed -i 's/"带宽监控"/"监控"/g' `grep "带宽监控" -rl ./`
+sed -i 's/"Argon 主题设置"/"Argon设置"/g' `grep "Argon 主题设置" -rl ./`
+
+# echo 'K3专用，编译K3的时候只会出K3固件（去掉sed前面的#生效）'
+# sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
+# echo '=========Build K3 only OK!========='
+
+
+
+# 以下是备用自定义配置，去'#'后才会执行，非必要不添加
+
+# echo '修改默认主题'
+# sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom/g' feeds/luci/collections/luci/Makefile 
+# echo '=========Alert Default theme OK!========='
+
+# echo '修改upnp绑定文件位置'
+# sed -i 's/\/var\/upnp.leases/\/tmp\/upnp.leases/g' feeds/packages/net/miniupnpd/files/upnpd.config
+# cat feeds/packages/net/miniupnpd/files/upnpd.config |grep upnp_lease_file
+# echo '=========Alert upnp binding file directory!========='
+
+# 修改NTP设置
+# sed -i "s/'0.openwrt.pool.ntp.org'/'ntp1.aliyun.com'/g" package/base-files/files/bin/config_generate
+# sed -i "s/'1.openwrt.pool.ntp.org'/'ntp2.aliyun.com'/g" package/base-files/files/bin/config_generate
+# sed -i "s/'2.openwrt.pool.ntp.org'/'ntp3.aliyun.com'/g" package/base-files/files/bin/config_generate
+# sed -i "s/'3.openwrt.pool.ntp.org'/'ntp4.aliyun.com'/g" package/base-files/files/bin/config_generate
+# cat package/base-files/files/bin/config_generate |grep system.ntp.server=
+# echo 'Alert NTP Settings OK!====================='
