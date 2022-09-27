8c6e2d9c8f53a0a3471c6777b0d9647fbbc0e604
anjue39 <98279987+anjue39@users.noreply.github.com>
2022-09-28 04:59:16 +0800

    Update diy-part1-x86.sh

--- diy-part1-x86.sh
+++ diy-part1-x86.sh
@@ -44,14 +44,14 @@ sed -i 's/ <%=luci.sys.exec("cat \/etc\/bench.log") or ""%>//g' package/lean/aut
 echo '=========Remove benchmark display in index OK!========='
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间（根据编译机型变化,自行调整需要删除的固件名称）
-cat >${GITHUB_WORKSPACE}/Clear <<-EOF
+# cat >${GITHUB_WORKSPACE}/Clear <<-EOF
 # rm -rf packages
-rm -rf config.buildinfo
-rm -rf feeds.buildinfo
-rm -rf openwrt-x86-64-generic-kernel.bin
-rm -rf openwrt-x86-64-generic.manifest
-rm -rf openwrt-x86-64-generic-squashfs-rootfs.img.gz
-rm -rf sha256sums
-rm -rf version.buildinfo
-rm -rf openwrt-x86-64-generic-squashfs-combined-efi.img.gz
-EOF
+# rm -rf config.buildinfo
+# rm -rf feeds.buildinfo
+# rm -rf openwrt-x86-64-generic-kernel.bin
+# rm -rf openwrt-x86-64-generic.manifest
+# rm -rf openwrt-x86-64-generic-squashfs-rootfs.img.gz
+# rm -rf sha256sums
+# rm -rf version.buildinfo
+# rm -rf openwrt-x86-64-generic-squashfs-combined-efi.img.gz
+# EOF
