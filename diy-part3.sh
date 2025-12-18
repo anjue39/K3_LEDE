5a65ca94c90ad09ea12f94f9da1e63aac88f8bc4
anjue39 <98279987+anjue39@users.noreply.github.com>
2025-12-18 12:15:25 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # 清理旧的自定义 feeds 行（避免重复添加）
-sed -i '/^src-git openclash/d; /^src-git nikki/d' feeds.conf.default
+# sed -i '/^src-git openclash/d; /^src-git nikki/d' feeds.conf.default
 
-echo '添加自定义源'
+# echo '添加自定义源'
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
-sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki' feeds.conf.default
+# sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+# sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki' feeds.conf.default
 # sed -i '$a src-git ECH https://github.com/SunshineList/luci-app-ech-workers' feeds.conf.default
-echo '=========Add a feed source OK!========='
+# echo '=========Add a feed source OK!========='
 
 # echo '添加jerrykuku的argon主题及设置'
 # rm -rf package/lean/luci-theme-argon package/lean/luci-app-argon-config  
